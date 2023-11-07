bab42e705c8b9d770700386cd3b39bb410658b8a
한상희 <tkdgml822@gmail.com>
2023-11-07 17:41:00 +0900

    edit HelloLombok

--- src/main/java/com/mysite/sbb/HelloLombok.java
+++ src/main/java/com/mysite/sbb/HelloLombok.java
@@ -2,18 +2,19 @@ package com.mysite.sbb;
 
 
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 public class HelloLombok {
-    private String hello;
-    private int lombok;
+    private final String hello;
+    private final int lombok;
+
+    public HelloLombok(String hello, int lombok) {
+        this.hello = hello;
+        this.lombok = lombok;
+    }
 
     public static void main(String[] args) {
-        HelloLombok helloLombok = new HelloLombok();
-        helloLombok.setHello("헬로");
-        helloLombok.setLombok(5);
+        HelloLombok helloLombok = new HelloLombok("헬로", 5);
 
         System.out.println(helloLombok.getHello());
         System.out.println(helloLombok.getLombok());
