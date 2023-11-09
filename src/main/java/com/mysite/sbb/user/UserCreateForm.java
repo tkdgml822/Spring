c93091894899058267ded67bf262a907a7aa747c
한상희 <tkdgml822@gmail.com>
2023-11-10 08:23:16 +0900

    회원가입 추가

--- src/main/java/com/mysite/sbb/user/UserCreateForm.java
+++ src/main/java/com/mysite/sbb/user/UserCreateForm.java
@@ -16,10 +16,11 @@ public class UserCreateForm {
     @NotEmpty(message = "비밀번호는 필수항목입니다.")
     private String password1;
 
-    @NotEmpty(message = "비밀번호 재입력은 필수항목입니다.")
-    private String password;
+    @NotEmpty(message = "비밀번호 확인은 필수항목입니다.")
+    private String password2;
 
     @NotEmpty(message = "이메일은 필수항목입니다.")
     @Email
     private String email;
+
 }
