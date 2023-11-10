b600e4ae8be07898066a5ca75d7bcadb9ed3acb6
한상희 <tkdgml822@gmail.com>
2023-11-11 01:49:05 +0900

    마크다운으로 글 설정

--- src/main/java/com/mysite/sbb/question/Question.java
+++ src/main/java/com/mysite/sbb/question/Question.java
@@ -5,11 +5,17 @@ import java.util.List;
 import java.util.Set;
 
 import com.mysite.sbb.answer.Answer;
-
 import com.mysite.sbb.user.SiteUser;
-import jakarta.persistence.*;
-import jakarta.persistence.ManyToOne;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -37,7 +43,6 @@ public class Question {
 
     private LocalDateTime modifyDate;
 
-    // 추천인 중복되면 안된다 set쓴 이유.
     @ManyToMany
     Set<SiteUser> voter;
 }
--- src/main/java/com/mysite/sbb/question/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import com.mysite.sbb.user.SiteUser;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -13,6 +12,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.mysite.sbb.DataNotFoundException;
+import com.mysite.sbb.user.SiteUser;
 
 import lombok.RequiredArgsConstructor;
 
@@ -22,8 +22,11 @@ public class QuestionService {
 
     private final QuestionRepository questionRepository;
 
-    public List<Question> getList() {
-        return this.questionRepository.findAll();
+    public Page<Question> getList(int page) {
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate"));
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+        return this.questionRepository.findAll(pageable);
     }
 
     public Question getQuestion(Integer id) {
@@ -33,13 +36,6 @@ public class QuestionService {
         } else {
             throw new DataNotFoundException("question not found");
         }
-    }
-
-    public Page<Question> getList(int page) {
-        List<Sort.Order> sorts = new ArrayList<>();
-        sorts.add(Sort.Order.desc("createDate"));
-        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
-        return this.questionRepository.findAll(pageable);
     }
 
     public void create(String subject, String content, SiteUser user) {
